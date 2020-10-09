463229214b77796480b2b61668b85b2dc4032e7c
blacktiger141 <72620290+blacktiger141@users.noreply.github.com>
2020-10-09 17:31:48 +0000

    Create Bng.py

---
+++ Bng.py
@@ -0,0 +1,68 @@
+https://github.com/blacktiger141
+  <h1 align="center">Bngv1.0</h1>
+ <p align="center">
+       A new international facebook account cracker tool for termux users
+ </p>
+
+  ##  ***About Bng***:
+
+  Bng is a python based script. You can use this tool for crack facebook users passwords. This tool works on both rooted Android device and Non-rooted Android device.
+
+  ## All commands for install this tool
+ $ pkg update && pkg upgrade
+ <br>
+ $ pkg install python
+ <br/>
+ $ pkg install python2
+ <br/>
+ $ pkg install git
+ <br/>
+ $ pkg install wget
+ <br/>
+ $ pkg install php
+ <br/>
+ $ pkg install openssh
+ <br/>
+ $ git clone https://github.com/botolmehedi/bint
+ <br/>
+ $ pip2 install requests
+ <br/>
+ $ pip2 install mechanize
+ <br/>
+ $ ls
+ <br/>
+ $ cd Bng
+ <br/>
+ $ python2 Bng.py
+ <br/>
+ ...
+ <br/>
+ • TOOL USER : (knock me on facebook)
+ <br/>
+ • TOOL PASS : (knock me on facebook)
+ <br/>
+ ....
+ <br/>
+
+  * Note:- Don't try to edit or modify this tool.
+
+  ##  ***Check this***
+
+  ##
+
+ 
+
+  ##  ***Join***
+
+ 
+  ### Facebook page:
+ https://www.facebook.com/Md Shiam
+
+
+
+  ### My GitHub ID link:https://github.com/blacktiger141
+  
+
+  ### Warning
+
+  ***This tool is only for educational purpose. If you use this tool for other purposes except education we will not be responsible in such cases.***
